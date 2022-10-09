633968d5f7ee77f37d87186013ad3c222efc88be
Rasmus Pallisgaard <rasm796h@gmail.com>
2022-10-10 00:07:17 +0200

    didnt work

--- models/ConvNext.py
+++ models/ConvNext.py
@@ -71,5 +71,4 @@ def get_convnext_trainer(gpus=1,
                       callbacks=callbacks,
                       logger=logger,
                       strategy="ddp",
-                      find_unused_parameters=True,
                       enable_progress_bar=False)

c267d2b11e5a62613acd44cf003945b615bbb7d2
Rasmus Pallisgaard <rasm796h@gmail.com>
2022-10-16 14:47:23 +0200

    added test script so we can figure out whats wrong

--- test_script.sh
+++ test_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The partition is the queue you want to run on. standard is gpu and can be ommitted.
-#SBATCH -p gpu --gres=gpu:titanrtx:4
+#SBATCH -p gpu --gres=gpu:titanrtx:1
 #SBATCH --job-name=ATIAConvNext
 #number of independent tasks we are going to start in this script
 #SBATCH --ntasks=1
 #number of cpus we want to allocate for each program
 #SBATCH --cpus-per-task=2
 #the ammount of memory allocated
-#SBATCH --mem=12000M
+#SBATCH --mem=2000M
 #We expect that our program should not run longer than 2 days
 #Note that a program will be killed once it exceeds this time!
 #SBATCH --time=4:00:00
@@ -15,3 +15,5 @@
 # From here on, we can start our program
 python --version > echo
 echo $CUDA_VISIBLE_DEVICES
+
+python testing_python.py

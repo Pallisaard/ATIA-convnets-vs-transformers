7156916d13a0afbb3f8702c78eb9c013bad23d01
Rasmus Pallisgaard <rasm796h@gmail.com>
2022-10-18 10:29:39 +0200

    WORKS! Now lets do 10 of those badboys

--- main_swin_cifar.sh
+++ main_swin_cifar.sh
@@ -3,8 +3,8 @@
 #SBATCH -p gpu --gres=gpu:titanrtx:4
 #SBATCH --job-name=atia-swin-cif10
 #number of independent tasks we are going to start in this script
-# comment on --array 1-10%3
-#SBATCH --ntasks=1
+#SBtACH --array 1-10%3
+# comment on --ntasks=1
 #number of cpus we want to allocate for each program
 #SBATCH --cpus-per-task=6
 #the ammount of memory allocated
